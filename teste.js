ca9ceadadce6bb07306d6b77193094f69a4b8d7f
Victor Pereira <victorpereirademelo18@gmail.com>
2022-01-19 18:58:16 -0300

    Finalizar a parte de Editar Post

--- teste.js
+++ teste.js
@@ -1,18 +1,29 @@
 // remover element
 
-const guiasFiltradas = dados.filter(dado => {
-    const titleNormalizado = dado.title.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+// const guiasFiltradas = dados.filter(dado => {
+//     const titleNormalizado = dado.title.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "");
 
-    if (buscaNormalizada && titleNormalizado.includes(buscaNormalizada)) {
-        return false;
-    }
+//     if (buscaNormalizada && titleNormalizado.includes(buscaNormalizada)) {
+//         return false;
+//     }
 
-    return true;
-});
+//     return true;
+// });
 
-dados[0].title = `Victor Pereira`
-console.log(dados[0]);
-dados[1].title = `Augusto`
+// dados[0].title = `Victor Pereira`
+// console.log(dados[0]);
+// dados[1].title = `Augusto`
 
 // Usar uma promisse fetch ou axios com ela vou ter uma variavel imutavel pois estara puxando um dado externo , vou ta empurrando esses dados para outra variavel para armazenar
 //esses dados e com ele vou alterar os dados (usando o filter para excluir) com o modal e quando eu for atualizar a lista vou retornar aos dados inicial
+
+// const array1 = [5, 12, 8, 130, 44];
+// const found = array1.find(element => element = 5);
+// console.log(found);
+// expected output: 12
+
+arr = [1,2,3,4]
+
+arr[2] = 5
+
+console.log(arr)
